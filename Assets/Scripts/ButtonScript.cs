3ddbfbe3d44959a3d7fd89a58eda35eb7b46ed59
Jack <jackdavidhaseldine@hotmail.com>
2019-11-13 20:01:45 +0000

    Initial Commit

---
+++ Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonScript : MonoBehaviour
+{
+    public Button button;
+
+    public void ChangeColour()
+    {
+        var colours = button.colors;
+        colours.normalColor = InvertColour(colours.normalColor);
+        colours.highlightedColor = InvertColour(colours.highlightedColor);
+        colours.disabledColor = InvertColour(colours.disabledColor);
+        colours.pressedColor = InvertColour(colours.pressedColor);
+        colours.selectedColor = InvertColour(colours.selectedColor);
+        button.colors = colours;
+    }
+
+    Color InvertColour(Color ColourToInvert)
+    {
+        const int RGBMAX = 1;
+        return new Color(RGBMAX - ColourToInvert.r, RGBMAX - ColourToInvert.g, RGBMAX - ColourToInvert.b);
+    }
+}
---
+++ Assets/Scripts/GameScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameScript : MonoBehaviour
+{
+    public GameObject GameBoard;
+
+    public GameObject[] GameRows;
+
+    public Button[,] Buttons;
+
+    public int numberOfRows = 8, numberOfColumns = 8;
+
+    private void Start()
+    {
+        Buttons = new Button[numberOfColumns, numberOfRows];
+        for(int i=0;i<GameRows.Length;i++)
+        {
+            Button[] rowButtons = GameRows[i].GetComponentsInChildren<Button>();
+            for(int j=0;j<rowButtons.Length;j++)
+            {
+                Buttons[j, i] = rowButtons[j];
+            }
+        }
+    }
+
+    public void ButtonPressed(int which)
+    {
+        int x = which % 8;
+        int y = which / 8;
+
+        CallButton(x, y);
+        CallButton(x+1, y);
+        CallButton(x-1, y);
+        CallButton(x, y+1);
+        CallButton(x, y-1);
+
+    }
+
+    public void CallButton(int x, int y)
+    {
+        if(x >= 0 && x < numberOfColumns && y >= 0 && y < numberOfRows)
+        {
+            ButtonScript bs = Buttons[x, y].GetComponent<ButtonScript>();
+            bs.ChangeColour();
+        }
+    }
+
+}
